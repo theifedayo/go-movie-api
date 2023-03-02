1f16116909b25bf2313718c937cc7faca0bcd59c
theifedayo <adesiyanifedayo@gmail.com>
2023-03-02 10:36:38 +0100

    setup postgres database

---
+++ README.md
@@ -0,0 +1,1 @@
+### go-movie-api
---
+++ config/database.go
@@ -0,0 +1,15 @@
+package config
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+
+func LoadEnvVariables(){
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(" Error loading .env files ")
+	}
+}
